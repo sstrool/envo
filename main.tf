a4529191bd8ae7cb4c9f9ceab83eb4657ac4bcd0
Scott Strool <scott.strool@gmail.com>
2023-08-01 16:33:17 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,5 @@ resource "aws_s3_bucket" "b" {
   bucket = "my-ss-bucket" 
   force_destroy = true
 
-
-  
 }
 
-resource "aws_budgets_budget" "my-budget-name" {
-    name = "monthly_budget"
-    budget_type = "COST"
-    limit_amount = "1.00"
-    limit_unit = "USD"
-    time_unit = "MONTLY"
-    time_period_start = "2023-07-01 00:01"
-  }
